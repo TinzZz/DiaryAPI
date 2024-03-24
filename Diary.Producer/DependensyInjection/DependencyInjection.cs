900bdcaa9478b0472b1074f61baffd4ca6757683
TinzZz <sashasportsmen@mail.ru>
2024-03-24 21:27:42 +0300

    Trying to add RabbitMq to proj

---
+++ Diary.Producer/DependensyInjection/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Diary.Producer.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary.Producer.DependensyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void AddProducer(this IServiceCollection services)
+        {
+            services.AddScoped<IMessageProducer, Producer>();
+        }
+    }
+}
